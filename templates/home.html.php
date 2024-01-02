576723b4bb5164eac108e17f781df867352b0211
ghenadie gandrabur <ghenadiegandrabur@gmail.com>
2024-01-02 17:05:27 +0200

    advertising 2

--- templates/home.html.php
+++ templates/home.html.php
@@ -86,17 +86,8 @@
                 </div>
             </a>
 
+            <?php include __DIR__."/../includes/advertising.php" ?>
         </div>
         <div class="col-s-2 col-2"></div>
     </div>
 </div>
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7188583604227224"
-     crossorigin="anonymous"></script>
-<!-- Tahograf header -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:728px;height:90px"
-     data-ad-client="ca-pub-7188583604227224"
-     data-ad-slot="6541643188"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
--- templates/tahograf.html.php
+++ templates/tahograf.html.php
@@ -39,14 +39,5 @@
     <?php endforeach; ?>
   </div>
 </div>
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7188583604227224"
-     crossorigin="anonymous"></script>
-<!-- Tahograf header -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:728px;height:90px"
-     data-ad-client="ca-pub-7188583604227224"
-     data-ad-slot="6541643188"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
 <script src="/myjs/modal.js"></script>
+<?php include __DIR__."/../includes/advertising.php" ?>
