d0548688c8601816987a75e8649f624ed064d76f
ghenadie gandrabur <ghenadiegandrabur@gmail.com>
2024-01-02 18:21:04 +0200

    version 01

--- templates/layout.html.php
+++ templates/layout.html.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
-
 <head>
+	<!-- Version 01 -->
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -42,7 +42,7 @@
 	<title><?= $title ?></title>
 </head>
 
-<body>
+<body>	
 	<?php include 'menu.html.php' ?>
 
 	<?= $output ?>
